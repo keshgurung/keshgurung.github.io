41d3839eed38f432879403f7cdd23c3936b391f3
kesh gurung <k_grg@hotmail.com>
2022-01-18 21:40:25 +0000

    deploy

--- script.js
+++ script.js
@@ -36,14 +36,14 @@ $(document).ready(function () {
 
   // typing text animation script
   var typed = new Typed('.typing', {
-    strings: ['Web Developer'],
+    strings: ['Web Developer.'],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
   })
 
   var typed = new Typed('.typing-2', {
-    strings: ['Web Developer'],
+    strings: ['Web Developer.'],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
